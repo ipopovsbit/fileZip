12e55334e3480371320a6ac9ac6173de55d5f3a4
taurus <igorpopov10@yahoo.com>
2019-06-17 09:28:25 +0200

    Update ConfigurationUtility.java

--- src/main/java/zipping/co/ConfigurationUtility.java
+++ src/main/java/zipping/co/ConfigurationUtility.java
@@ -1,35 +1,30 @@
 package zipping.co;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationUtility {
-	private static Properties properties;
-	private static Properties parentProperties;
-
-	static {
+	
+      private static Properties properties;
+      private static Properties parentProperties;
+	
+      static {
 		properties = new Properties();
 		try {
 			properties.load(new FileInputStream("resources/conf.properties"));
-
 			if (properties.getProperty("parentConf") != null) {
 				parentProperties.load(new FileInputStream(properties.getProperty("parentConf")));
 			}
-
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
-
 		}
 	}
 
 	public static String getProperty(String propName) {
-
 		if (properties.getProperty(propName) != null)
 			return properties.getProperty(propName);
 		if (parentProperties != null)
 			return parentProperties.getProperty(propName);
 		return null;
 	}
-
 }
